eb10e1015f26c3cd6b74edb3dba4c4da751a5adc
ptfn <CipherDogs@protonmail.com>
2021-04-05 21:40:03 +0700

    fix code

--- Password.py
+++ Password.py
@@ -1,12 +1,13 @@
 import random
 import sys
 
+big_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+small_char = "abcdefghijklmnopqrstuvwxyz"
+number = "0123456789"
+symbols = "!?@#$%^&*=<>()[]/|,.+-_"
+
 
 def main():
-    big_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    small_char = "abcdefghijklmnopqrstuvwxyz"
-    number = "0123456789"
-    symbols = "!?@#$%^&*=<>()[]/|,.+-_"
     alphabet = ""
     
     print("██████╗  █████╗ ███████╗███████╗ ██████╗ ███████╗███╗   ██╗")
@@ -21,7 +22,11 @@ def main():
         print('Error code!')
         exit(0)
     else:
-        pass
+        alphabet = brut_alph(choice)
+
+    length = int(input("Length passwords:"))
+    password = pass_gen(length, alphabet)
+    print("Password -> {}".format(password))
 
 
 def pass_gen(length, symbols):
@@ -29,6 +34,26 @@ def pass_gen(length, symbols):
     for i in range(length):
         password += random.choice(symbols)
     return password
+
+
+def brut_alph(string):
+    str_last = ""
+    str_res = ""
+    for i in range(len(string)):
+        if string[i] not in str_last:
+            if string[i] == "b":
+                str_res += big_char
+                str_last += "b"
+            if string[i] == "с":
+                str_res += small_char
+                str_last += "с"
+            if string[i] == "n":
+                str_res += number
+                str_last += "n"
+            if string[i] == "s":
+                str_res += symbols
+                str_last += "s"
+    return str_res
 
 
 if __name__ == "__main__":
