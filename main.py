6abfd1ac23e5f1f6cf05691bb89e6148ab4792e4
ptfn <CipherDogs@protonmail.com>
2022-09-17 20:05:21 +0700

    updated code

--- main.py
+++ main.py
@@ -1,84 +1,101 @@
-import random
-import hashlib
-import base64
+from random import randint, choice
+from colorama import Fore, Style
+from base64 import b64encode
+from hashlib import sha512
+from math import log
+import argparse
 
-chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@#$%^&*=<>()[]/|,.+-_"
-
-
-def main():
-    print("----- \033[32mPassGen\033[0m -----")
-    while True:
-        chc = input("Enter code:\n1) Password\n2) Help\n3) Exit\n:")
-        
-        if chc == "Password" or chc == "password" or chc == "pass" or chc == "1":
-            opt = input("Choose a way to generate your password:\n1) Random\n2) Hash\n:")
-
-            if opt == "hash" or opt == "Hash" or opt == "2":
-                text = input("Enter text\n:")
-                
-                if text == "":
-                    print("\033[31m{}\033[0m\n".format("The text is not visible!"))
-                    # exit(0)
-                
-                else:
-                    leng = int(input("Length passwords (max 64)\n:"))
-
-                    if leng <= 0:
-                        print("\033[31m{}\033[0m\n".format("Incorrect password length!"))
-                        # exit(0)
-                    
-                    else:
-                        password = pass_hash(leng, text)
-                        print("Password -> \033[32m{}\033[0m\n".format(password))
-            
-            elif opt == "Random" or opt == "random" or opt == "rand" or opt == "1":
-                leng = int(input("Length passwords\n:"))
-
-                if leng <= 0:
-                    print("033[31m{}\033[0m\n".format("Incorrect password length!"))
-                    # exit(0)
-                
-                else:
-                    password = pass_rand(leng)
-                    print("Password -> \033[32m{}\033[0m\n".format(password))
-
-            else:
-                print("\033[31m{}\033[0m\n".format("Error option!"))
-                # exit(0)
-
-        elif chc == "Help" or chc == "help" or chc == "2":
-            print("The utility is designed to generate a password. To generate a password, select 'Password'\n")
-
-        elif chc == "Exit" or chc == "exit" or chc == "quit" or chc == "Quit" or chc == "3":
-            break
-
-        else:
-            print("\033[31m{}\033[0m\n".format("Error choice!"))
+chars = """ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopq\
+rstuvwxyz0123456789!?@#$%^&*=<>()[]/|,.+-_"'"""
 
 
-def pass_hash(length, text):
-    password = ""
-    key = ""
-    string = ""
-    
-    for i in range(len(text)):
-        key += random.choice(chars)
-    
-    string = password + text + key
-    password = hashlib.sha1(str(string).encode("ascii")).hexdigest()
-    encbyte = base64.b64encode(password.encode("utf-8"))
-    encstr = str(encbyte, "utf-8")
-    
-    return encstr[:length]
+def crt(string):
+    char = {}
+    for i in range(len(string)):
+        try:
+            char[string[i]] += 1
+        except Exception:
+            char[string[i]] = 1
+    arr = [char[i] for i in char]
+    return arr
+
+
+def pass_ent(arr):
+    total = sum(arr)
+    entropy = 0
+    for i in range(len(arr)):
+        p = arr[i] / total
+        if p > 0:
+            entropy -= p * log(p, 2)
+    return entropy / 8 * 100
+
+
+def pass_lvl(num):
+    if num < 37.5:
+        return txt_rd('Bad')
+    elif num < 40:
+        return txt_yel('Weak')
+    elif num < 50:
+        return txt_grn('Good')
+    else:
+        return txt_grn('Excellent')
+
+
+def pass_hash(string, lenght):
+    for i in range(randint(2, 128)):
+        string = sha512(str(string).encode("ascii")).hexdigest()
+    encbyte = b64encode(string.encode("utf-8"))
+    encstr = str(encbyte, "utf-8")[:lenght]
+    return encstr
 
 
 def pass_rand(length):
     password = ""
-    
     for i in range(length):
-        password += random.choice(chars)
-    
+        password += choice(chars)
     return password
+
+
+def txt_grn(text):
+    return Fore.GREEN + text + Style.RESET_ALL
+
+
+def txt_rd(text):
+    return Fore.RED + text + Style.RESET_ALL
+
+
+def txt_yel(text):
+    return Fore.YELLOW + text + Style.RESET_ALL
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Password Generate')
+    parser.add_argument('-l', '--length', type=int,
+                        dest='leng', help='Length password')
+    parser.add_argument('-r', '--random', action='store_true',
+                        dest='random', help='Random algorithm')
+    parser.add_argument('-hs', '--hash', action='store_true',
+                        dest='hash', help='Hash algorithm')
+
+    args = parser.parse_args()
+    leng = args.leng
+    random = args.random
+    hsh = args.hash
+
+    if random:
+        password = pass_rand(leng)
+        entropy = round(pass_ent(crt(password)), 4)
+        level = pass_lvl(entropy)
+        print(f"Password\t{txt_grn(password)}\
+\nEntropy\t\t{txt_grn(level)} ({entropy})")
+    elif hsh:
+        password = pass_hash(pass_rand(randint(1, leng)), leng)
+        entropy = round(pass_ent(crt(password)), 4)
+        level = pass_lvl(entropy)
+        print(f"Password\t{txt_grn(password)}\
+\nEntropy\t\t{txt_grn(level)} ({entropy})")
+    else:
+        print(txt_rd("Error!"))
 
 
 if __name__ == "__main__":
